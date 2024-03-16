8b7f7fdc54e20c76ca9fa2e5dbe8844e158f6f26
Nikhil Verma <vermanick75@gmail.com>
2024-03-16 13:48:45 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+import "./App.css";
+import React, { useState, useEffect } from 'react';
+import Sudoku from "./Sudoku";
+
+function App() {
+  const [showTitle, setShowTitle] = useState(true);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setShowTitle(prevState => !prevState);
+    }, 2000); // Change the interval duration as needed (2000ms = 2 seconds)
+
+    return () => clearInterval(interval);
+  }, []);
+  return (
+    <div className="App">
+      {showTitle ? (
+        <h1 id="title">Sodoku Solver</h1>
+      ) : (
+        <h1 id="title">-Nikhil Verma</h1>
+      )}
+      <Sudoku />
+    </div>
+  );
+}
+
+export default App;
